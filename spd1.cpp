485854bfc0e58e367887a2bffe1789be028117e5
mkinas888 <mkinas888@gmail.com>
2019-03-02 23:34:40 +0100

    partly done calier method + minor changes done while editing

--- spd1.cpp
+++ spd1.cpp
@@ -7,11 +7,13 @@ using namespace std;
 class Task {
 public:
     int r, p, q;
+    int C = r+p+q;
 
     Task & operator = (const Task& task) {
         this->r = task.r;
         this->p = task.p;
         this->q = task.q;
+        this->C = task.C;
         return * this;
     }
 };
@@ -41,7 +43,7 @@ void showpq(priority_queue <Task, vector<Task>, compareR> pq)
     cout << endl; 
 } 
 
-void schrageMethod(int n, Task taskTable[], Task piTable[]) {
+int schrageMethod(int n, Task taskTable[], Task piTable[]) {
     Task e;
     int t=0, k=0, Cmax=0;
     priority_queue<Task, vector<Task>, compareR> N;
@@ -51,7 +53,6 @@ void schrageMethod(int n, Task taskTable[], Task piTable[]) {
     }
     while(!(G.empty()) || !(N.empty())) {
         while(!(N.empty()) && N.top().r <= t) {
-            cout << N.top().r << endl;
             e = N.top();
             G.push(e);
             N.pop();
@@ -72,19 +73,61 @@ void schrageMethod(int n, Task taskTable[], Task piTable[]) {
         Cmax = max(Cmax, t + e.q);
         k = k+1;
     }
+    return Cmax;
 }
+
+int calierMethod(int n, Task taskTable[], int UB) {
+    int U, a=0, b=0, c=-1, suma;
+    Task piTableX[n-1];
+    U = schrageMethod(n, taskTable, piTable);
+    if(U < UB) {
+        UB = U;
+        for(int i = 0;i<n;i++) {
+            piTableX[i] = piTable[i];
+        }
+    }
+    for(int i=n-1;i>0;i--) {
+        if (U == piTable[i].C + piTable[i].q)
+            {
+                b = i;
+                break;
+            }
+    }
+    for(int a=0;a<b;a++) {
+        suma=0;
+        for(int i=1; i<=b;i++) {
+            suma += piTable[i].p;
+        }
+        if(U == (piTable[a].r + suma + piTable[b].q)) {
+            break;
+        }
+    }
+    for(int i = b; i >= a; i--) {
+        if(piTable[i].q < piTable[b].q) {
+            c=i;
+            break;
+        }
+    }
+    if(c == -1) {
+        return U;
+    }
+}
+
+Task piTable[23];
 
 int main () {
     string s;
     ifstream data;
     data.open("rpq.data.txt");
     int n=24, UB;
-    Task taskTable[n-1], piTable[n-1];
+    Task taskTable[n-1];
     for(int i=0;i<24;i++) {
         data >> taskTable[i].r >> taskTable[i].p >> taskTable[i].q;
     }
-    schrageMethod(n, taskTable, piTable);
-    for(int o=0;o<24;o++){
-        cout << piTable[o].r << " " << piTable[o].p << " " << piTable[o].q <<  endl;
-    }
+    calierMethod(n, taskTable, 100000);
+    // UB = schrageMethod(n, taskTable, piTable);
+    // for(int o=0;o<24;o++){
+    //     cout << piTable[o].r << " " << piTable[o].p << " " << piTable[o].q <<  endl;
+    // }
+    // cout << UB << endl;
 }
