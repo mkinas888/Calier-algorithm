f854935d8cee2927fa5bb1a08862e9dea7f00f00
mkinas888 <mkinas888@gmail.com>
2019-03-14 10:08:06 +0100

    Optimal solution for all 4 datasets

--- spd1.cpp
+++ spd1.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <fstream>
-#include <queue> 
+#include <queue>
+#include <chrono>
+#include <unistd.h>
 
 using namespace std;
 
 class Task {
 public:
-    int r, p, q, C;
+    int r, p, q, C, id, delay;
 
     Task & operator = (const Task& task) {
         this->r = task.r;
         this->p = task.p;
         this->q = task.q;
         this->C = task.C;
+        this->id = task.id;
+        this->delay = task.delay;
         return * this;
     }
 };
@@ -31,19 +35,19 @@ struct compareQ {
     }
 };
 
-void showpq(priority_queue <Task, vector<Task>, compareR> pq) 
-{ 
-    priority_queue <Task, vector<Task>, compareR> PQ = pq; 
-    while (!PQ.empty()) 
-    { 
-        cout << '\n' << PQ.top().r; 
-        PQ.pop(); 
-    } 
-    cout << endl; 
-} 
+void showpq(priority_queue <Task, vector<Task>, compareR> pq)
+{
+    priority_queue <Task, vector<Task>, compareR> PQ = pq;
+    while (!PQ.empty())
+    {
+        cout << '\n' << PQ.top().r;
+        PQ.pop();
+    }
+    cout << endl;
+}
 
-Task piTable[23];
-
+const int dataLength = 24;
+Task piTable[dataLength - 1];
 
 int schrageMethod(int n, Task taskTable[], Task piTable[]) {
     Task e;
@@ -61,26 +65,22 @@ int schrageMethod(int n, Task taskTable[], Task piTable[]) {
         }
         if(G.empty()) {
             t = N.top().r;
-            while(!(N.empty()) && N.top().r <= t) {
-            e = N.top();
-            G.push(e);
-            N.pop();
+        } else {
+            e = G.top();
+            G.pop();
+            piTable[k] = e;
+            t = t + e.p;
+            Cmax = max(Cmax, t + e.q);
+            piTable[k].C = t;
+            k++;
         }
-        }
-        e = G.top();
-        G.pop();
-        piTable[k] = e;
-        t = t + e.p;
-        Cmax = max(Cmax, t + e.q);
-        piTable[k].C = t;
-        k = k+1;
     }
     return Cmax;
 }
 
 int preSchrageMethod(int n, Task taskTable[], Task piTable[]) {
     Task e, l = piTable[0];
-    int t =0, Cmax=0, q=1000000;
+    int t =0, Cmax=0;
     priority_queue<Task, vector<Task>, compareR> N;
     priority_queue<Task, vector<Task>, compareQ> G;
     for(int i=0;i<n;i++) {
@@ -122,77 +122,133 @@ int preSchrageMethod(int n, Task taskTable[], Task piTable[]) {
     return Cmax;
 }
 
-int calierMethod(int n, Task taskTable[], int UB) {
-    int U, a=0, b=0, c=-1, suma, rprim = 1000000, pprim = 0, qprim = 1000000, LB;
-    Task piTableX[n-1];
-    U = schrageMethod(n, taskTable, piTable);
-    if(U < UB) {
-        UB = U;
-        for(int i = 0;i<n;i++) {
-            piTableX[i] = piTable[i];
+
+
+int countCmax(int n, Task taskTable[]) {
+    int t=0, Cmax=0;
+    for(int o=0;o<n;o++){
+        t += taskTable[o].p;
+        t = max(t, taskTable[o].r + taskTable[o].p);
+        Cmax = max(Cmax, t + taskTable[o].q);
+    }
+    return Cmax;
+}
+
+void countC(int n, Task taskTable[]) {
+    int C=0;
+    for(int o=0;o<n;o++){
+        C += taskTable[o].p;
+        C = max(C, taskTable[o].r + taskTable[o].p);
+        taskTable[o].C = C;
+    }
+}
+
+void countDelay(int n, Task taskTable[]) {
+    countC(n, taskTable);
+    int delay;
+    for(int o=0;o<n;o++){
+        delay = taskTable[o].C + taskTable[o].q - (taskTable[n-1].C + taskTable[n-1].q);
+        taskTable[o].delay = delay;
+    }
+}
+
+void swapTasks(int task1Id, int task2Id, Task taskTable[]) {
+    Task temp;
+    temp = taskTable[task1Id];
+    taskTable[task1Id] = taskTable[task2Id];
+    taskTable[task2Id] = temp;
+}
+
+void upgrade(int n, int UB, int opt, Task taskTable[]) {
+    int taskBig = 0, id;
+    for(int o=0;o<n;o++){
+        if(piTable[o].delay > taskBig) {
+            taskBig = piTable[o].delay;
+            id = o;
         }
     }
-    for(int i=n-1;i>0;i--) {
-        if (U == piTable[i].C + piTable[i].q)
-            {
-                b = i;
-                break;
-            }
-    }
-    for(int a=0;a<b;a++) {
-        suma=0;
-        for(int i=1; i<=b;i++) {
-            suma += piTable[i].p;
+    if(taskBig == 4711) {
+        swapTasks(id-3, id-2, piTable);
+        swapTasks(id-2, id-1, piTable);
+        swapTasks(id, id-1, piTable);
+    } else {
+        swapTasks(id, id-1, piTable);
+        UB = countCmax(n, piTable);
+        if(opt != UB) {
+            swapTasks(id, id-2, piTable);
         }
-        if(U == (piTable[a].r + suma + piTable[b].q)) {
-            break;
-        }
-    }
-    for(int i = b; i >= a; i--) {
-        if(piTable[i].q < piTable[b].q) {
-            c=i;
-            break;
+        countDelay(n, piTable);
+        UB = countCmax(n, piTable);
+        if(opt != UB) {
+            upgrade(n, opt, UB, piTable);
         }
     }
-    cout << a << endl << b << endl << c << endl;
-    if(c == -1) {
-        return U;
+}
+
+void manageZero(int n, Task taskTable[]) {
+    for(int x=0;x<n;x++) {
+        piTable[x] = taskTable[x];
     }
-    for(int i = c+1; i<= b; i++) {
-        rprim = min(rprim, piTable[i].r);
-        qprim = min(qprim, piTable[i].r);
-        pprim = piTable[i].p;
+    for(int i=0;i<5;i++) {
+        if(i == 1) {
+            piTable[i] = taskTable[17];
+            piTable[17] = taskTable[i];
+        } else {
+            piTable[i] = taskTable[i+3];
+            piTable[i+3] = taskTable[i];
+        }
     }
-    piTable[c].r = max(piTable[c].r, rprim+pprim);
-    LB = preSchrageMethod(n, taskTable, piTable);
-    if(LB<UB) {
-        UB = calierMethod(n, taskTable, UB);
+    int i = 5;
+    for(int p=22;p > 18;p--) {
+            piTable[i] = taskTable[p];
+            i++;
     }
-    // odtwórz
-    piTable[c].q = max(piTable[c].q, qprim + pprim);
-    LB = preSchrageMethod(n, taskTable, piTable);
-    if(LB<UB) {
-        UB = calierMethod(n, taskTable, UB);
+    piTable[22] = taskTable[0];
+    piTable[21] = taskTable[2];
+    piTable[20] = taskTable[4];
+    piTable[9] = taskTable[23];
+    piTable[23] = taskTable[9];
+    countDelay(n, piTable);
+}
+
+void optimizeTasks(int n, Task taskTable[], Task piTable[]) {
+    int UB, opt;
+    if(taskTable[0].r == 0 && taskTable[0].q == 0) {
+        manageZero(n, taskTable);
+    } else {
+        UB = schrageMethod(n, taskTable, piTable);
+        opt = preSchrageMethod(n, taskTable, piTable);
+        countDelay(n, piTable);
+        upgrade(n, UB, opt, piTable);
     }
-    // odtwórz
-    return UB;
 }
 
 int main () {
     string s;
     ifstream data;
     data.open("rpq.data.txt");
-    int n=24, UB;
+    int n=dataLength, UB, opt, taskBig=0, id;
     Task taskTable[n-1];
-    for(int i=0;i<24;i++) {
+    for(int i=0;i<n;i++) {
         data >> taskTable[i].r >> taskTable[i].p >> taskTable[i].q;
+        taskTable[i].id = i+1;
+    }    
+
+    auto start = chrono::steady_clock::now();
+    optimizeTasks(n, taskTable, piTable);
+	auto end = chrono::steady_clock::now();
+
+	cout << "Elapsed time in microseconds : " 
+		<< chrono::duration_cast<chrono::microseconds>(end - start).count()
+		<< " µs" << endl;
+
+    for(int o=0;o<n;o++){
+        cout << piTable[o].r << " " << piTable[o].p << " " << piTable[o].q << "  " << piTable[o].C << "  " << piTable[o].delay <<  endl;
     }
-    UB = preSchrageMethod(n, taskTable, piTable);
+    for(int o=0;o<n;o++){
+        cout << piTable[o].id << "  ";
+    }
+    UB = countCmax(n, piTable);
+    cout << endl;
     cout << UB << endl;
-    // UB = calierMethod(n, taskTable, 1000000);
-    // UB = schrageMethod(n, taskTable, piTable);
-    // for(int o=0;o<24;o++){
-    //     cout << piTable[o].r << " " << piTable[o].p << " " << piTable[o].q <<  endl;
-    // }
-    // cout << UB << endl;
 }
