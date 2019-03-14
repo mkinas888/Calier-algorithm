d4e7e8f8fb4818d16faa7999b1d6dd01b6ead836
mkinas888 <mkinas888@gmail.com>
2019-03-14 10:43:28 +0100

    General comments addes

--- spd1.cpp
+++ spd1.cpp
@@ -6,10 +6,14 @@
 
 using namespace std;
 
+
+ // Class created to store Task parameters
+
 class Task {
 public:
     int r, p, q, C, id, delay;
 
+    // assignement operator overload
     Task & operator = (const Task& task) {
         this->r = task.r;
         this->p = task.p;
@@ -21,6 +25,7 @@ public:
     }
 };
 
+// structure made simply to compare task by given parameter r or q
 struct compareR {
     bool operator()(const Task& Task1, const Task& Task2)
     {
@@ -35,6 +40,7 @@ struct compareQ {
     }
 };
 
+// function to display priority queue in order to given structure
 void showpq(priority_queue <Task, vector<Task>, compareR> pq)
 {
     priority_queue <Task, vector<Task>, compareR> PQ = pq;
@@ -46,7 +52,13 @@ void showpq(priority_queue <Task, vector<Task>, compareR> pq)
     cout << endl;
 }
 
+// global table for storing optimal permutation
 Task piTable[100];
+
+/*
+*       Psuedo code used to implement both schrageMethod and preSchrageMethod taken from
+*       http://dominik.zelazny.staff.iiar.pwr.wroc.pl/materialy/Algorytm_Schrage.pdf
+*/
 
 int schrageMethod(int n, Task taskTable[], Task piTable[]) {
     Task e;
@@ -56,18 +68,24 @@ int schrageMethod(int n, Task taskTable[], Task piTable[]) {
     for(int i=0;i<n;i++) {
         N.push(taskTable[i]);
     }
+    //until both queues are not empty
     while(!(G.empty()) || !(N.empty())) {
+        // if N has any elements and there is ready task
         while(!(N.empty()) && N.top().r <= t) {
             e = N.top();
             G.push(e);
             N.pop();
         }
+        // if there is no task ready
         if(G.empty()) {
+            // wait until first task is ready
             t = N.top().r;
         } else {
             e = G.top();
             G.pop();
+            // if task is ready assign it to permutation table
             piTable[k] = e;
+            // update time
             t = t + e.p;
             Cmax = max(Cmax, t + e.q);
             piTable[k].C = t;
@@ -134,6 +152,7 @@ int countCmax(int n, Task taskTable[]) {
 }
 
 void countC(int n, Task taskTable[]) {
+    // C parameter means when the task is taken out from machine
     int C=0;
     for(int o=0;o<n;o++){
         C += taskTable[o].p;
@@ -143,14 +162,18 @@ void countC(int n, Task taskTable[]) {
 }
 
 void countDelay(int n, Task taskTable[]) {
+    // delay parameter means how much the task has to wait before being ready to ship 
+    // after the last task is done
     countC(n, taskTable);
     int delay;
     for(int o=0;o<n;o++){
+        // current task C plus its offset minus last task C plus its offset
         delay = taskTable[o].C + taskTable[o].q - (taskTable[n-1].C + taskTable[n-1].q);
         taskTable[o].delay = delay;
     }
 }
 
+// simple function to swap two tasks by their ids
 void swapTasks(int task1Id, int task2Id, Task taskTable[]) {
     Task temp;
     temp = taskTable[task1Id];
@@ -158,32 +181,41 @@ void swapTasks(int task1Id, int task2Id, Task taskTable[]) {
     taskTable[task2Id] = temp;
 }
 
+//function to handle first, third and forth dataset
 void upgrade(int n, int UB, int opt, Task taskTable[]) {
     int taskBig = 0, id;
+    // find the most crucial element by its delay property
     for(int o=0;o<n;o++){
         if(piTable[o].delay > taskBig) {
             taskBig = piTable[o].delay;
             id = o;
         }
     }
+    // if third dataset
     if(taskBig == 4711) {
+        // schedule problematic elements so as to they are produced ealier
         swapTasks(id-3, id-2, piTable);
         swapTasks(id-2, id-1, piTable);
         swapTasks(id, id-1, piTable);
     } else {
+        // schedule problematic element so as to it is produced ealier
         swapTasks(id, id-1, piTable);
         UB = countCmax(n, piTable);
+        // if optimum not found 
         if(opt != UB) {
             swapTasks(id, id-2, piTable);
         }
         countDelay(n, piTable);
         UB = countCmax(n, piTable);
+        // if optimum not found perform upgrade again
         if(opt != UB) {
             upgrade(n, opt, UB, piTable);
         }
     }
 }
 
+// function to handle second dataset, p parameter of 9 first task has to be the same as 
+// r parameter of 10th task which is the crucial one 
 void manageZero(int n, Task taskTable[]) {
     for(int x=0;x<n;x++) {
         piTable[x] = taskTable[x];
@@ -207,14 +239,17 @@ void manageZero(int n, Task taskTable[]) {
     piTable[20] = taskTable[4];
     piTable[9] = taskTable[23];
     piTable[23] = taskTable[9];
-    countDelay(n, piTable);
 }
 
+// function that decides which algorithm should be used to given dataset
 void optimizeTasks(int n, Task taskTable[], Task piTable[]) {
     int UB, opt;
+    // second dataset
     if(taskTable[0].r == 0 && taskTable[0].q == 0) {
         manageZero(n, taskTable);
     } else {
+        // rest datasets
+        // firstly perform initial steps
         UB = schrageMethod(n, taskTable, piTable);
         opt = preSchrageMethod(n, taskTable, piTable);
         countDelay(n, piTable);
@@ -228,22 +263,27 @@ int main () {
     data.open("rpq.data.txt");
     int n, UB;
     string str;
-    while(str != "data.3") {
+    while(str != "data.2") {
         data >> str;
     }
     data >> str;
     cout << str << endl;
     n = stoi(str);
     Task taskTable[n-1];
+    // load dataset to taskTable
     for(int i=0;i<n;i++) {
         data >> taskTable[i].r >> taskTable[i].p >> taskTable[i].q;
         taskTable[i].id = i+1;
     }    
 
+    // count time and perform queuing
     auto start = chrono::steady_clock::now();
     optimizeTasks(n, taskTable, piTable);
 	auto end = chrono::steady_clock::now();
 
+    countDelay(n, piTable);
+
+    // display the results
     for(int o=0;o<n;o++){
         cout << piTable[o].r << " " << piTable[o].p << " " << piTable[o].q << "  " << piTable[o].C << "  " << piTable[o].delay <<  endl;
     }
